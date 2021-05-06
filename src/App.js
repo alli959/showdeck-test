e8be60475db41dfc3bf566dbcc59e56f3a7fdd6d
alli959 <alexandergudmundsson@gmail.com>
2021-05-06 13:52:53 +0000

    It's a start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import UserPage from './components/UserPage';
 
 function App() {
+  const getData=()=>{
+    fetch('data.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      < UserPage />
     </div>
   );
 }
---
+++ src/components/UserPage.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+
+function UserPage() {
+
+    
+    return (
+        
+<div className="bg-white py-8 px-4 border-t-4 border-orange">
+    </div>
+
+)
+}
+
+export default UserPage;
